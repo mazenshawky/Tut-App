56b1d5b5027785a3039a4c94fc88bfcc0c585044
mazenshawky <mazenshawqy@gmail.com>
2022-09-18 23:12:44 +0200

    commit

---
+++ lib/presentation/forgot_password/viewmodel/forgot_password_viewmodel.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:advanced_app/app/functions.dart';
+import 'package:advanced_app/domain/usecase/forgot_password_usecase.dart';
+import 'package:advanced_app/presentation/base/baseviewmodel.dart';
+import 'package:advanced_app/presentation/common/state_renderer/state_renderer.dart';
+import 'package:advanced_app/presentation/common/state_renderer/state_renderer_impl.dart';
+
+class ForgotPasswordViewModel extends BaseViewModel
+    with ForgotPasswordViewModelInputs, ForgotPasswordViewModelOutputs {
+  final StreamController _emailStreamController =
+      StreamController<String>.broadcast();
+  final StreamController _isAllInputValidStreamController =
+      StreamController<void>.broadcast();
+
+  final ForgotPasswordUseCase _forgotPasswordUseCase;
+
+  ForgotPasswordViewModel(this._forgotPasswordUseCase);
+
+  var email = "";
+
+  //inputs
+  @override
+  void dispose() {
+    super.dispose();
+    _emailStreamController.close();
+    _isAllInputValidStreamController.close();
+  }
+
+  @override
+  void start() {
+    inputState.add(ContentState());
+  }
+
+  @override
+  Sink get inputEmail => _emailStreamController.sink;
+
+  @override
+  Sink get inputIsAllInputValid => _isAllInputValidStreamController.sink;
+
+  @override
+  forgotPassword() async {
+    inputState.add(
+        LoadingState(stateRendererType: StateRendererType.popupLoadingState));
+    (await _forgotPasswordUseCase.execute(email)).fold(
+        (failure) => {
+              inputState.add(ErrorState(
+                  StateRendererType.popupErrorState, failure.message))
+            }, (supportMessage) {
+      inputState.add(ContentState());
+    });
+  }
+
+  @override
+  setEmail(String email) {
+    inputEmail.add(email);
+    this.email = email;
+    _validate();
+  }
+
+  //outputs
+  @override
+  Stream<bool> get outIsEmailValid =>
+      _emailStreamController.stream.map((email) => isEmailValid(email));
+
+  @override
+  Stream<bool> get outIsAllInputValid => _isAllInputValidStreamController.stream
+      .map((isAllInputValid) => _isAllInputValid());
+
+  _isAllInputValid() {
+    return isEmailValid(email);
+  }
+
+  _validate() {
+    inputIsAllInputValid.add(null);
+  }
+}
+
+abstract class ForgotPasswordViewModelInputs {
+  setEmail(String email);
+
+  forgotPassword();
+
+  Sink get inputEmail;
+
+  Sink get inputIsAllInputValid;
+}
+
+abstract class ForgotPasswordViewModelOutputs {
+  Stream<bool> get outIsEmailValid;
+
+  Stream<bool> get outIsAllInputValid;
+}
